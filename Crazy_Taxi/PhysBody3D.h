b2d3be19f44206305f0738f3124237c15745c503
DidacRomero <36265669+DidacRomero@users.noreply.github.com>
2018-12-23 13:06:07 +0100

    Added Get Position function for physbodies

--- Crazy_Taxi/PhysBody3D.h
+++ Crazy_Taxi/PhysBody3D.h
@@ -2,6 +2,7 @@
 #define __PhysBody3D_H__
 
 #include "p2List.h"
+#include "glmath.h"
 
 class btRigidBody;
 class Module;
@@ -21,6 +22,7 @@ public:
 	
 	//Function to Set the rigidbody as a Sensor
 	void SetAsSensor(bool is_sensor);
+	vec3 GetPosition();
 
 private:
 	btRigidBody* body = nullptr;
